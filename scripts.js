998eeb1f3eb62559886f4b956c23cbc625096c9c
Ruslan <118926729+youngshame@users.noreply.github.com>
2025-03-05 21:21:37 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,17 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
             back: "Назад",
             about: "Обо мне",
             aboutme: "Привет! Я — Young Shame. Увлекаюсь веб-разработкой на любительском уровне, создаю различные проекты и всегда ищу что-то новое в этой сфере. Помимо разработки, я также люблю игры и музыку. В последнее время начал осваивать роль Achievement Hunter и рад зарабатывать трофеи в играх. Всё, что я делаю, вдохновляет меня и помогает развиваться в новых направлениях.",
-
+            back: "Назад"
         },
         en: {
             back: "Back",
             about: "About Me",
             aboutme: "Hi! I am a Young Shame. I am interested in web development at an amateur level, I create various projects and am always looking for something new in this field. Besides developing, I also love games and music. Recently, I have started to master the role of Achievement Hunter and am happy to earn trophies in games. Everything I do inspires me and helps me develop in new directions.",
+            back: "Back"
         }
     };
 
     const langBtns = document.querySelectorAll(".lang-btn");
-    const backButton = document.querySelector(".page-links a");
+    
     const pageButtons = document.querySelectorAll(".page-button");
     const elementsToTranslate = document.querySelectorAll("[data-translate]");
 
@@ -29,10 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
 
-        // Обновление текста кнопки "Назад"
-        if (backButton) {
-            backButton.textContent = texts[lang].back;
-        }
+        
 
         // Обновление текста всех страниц
         pageButtons.forEach((btn) => {
